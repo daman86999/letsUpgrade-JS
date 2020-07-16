765e1db2ad8dd427fd9cf02615c40ef948a1b28d
daman86999 <daman.086999@gmail.com>
2020-07-16 13:13:25 +0530

    day2 done

---
+++ 15-july-2020 Assignment/app.js
@@ -0,0 +1,14 @@
+//Q1
+let name = prompt("What is your name")
+
+console.log(name);
+
+//Q3
+let bol = confirm("Are you 21+")
+
+if(bol==true){
+    console.log("Can Drink")
+}
+else{
+    console.log("Can't Drink")
+}
